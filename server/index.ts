1c95cc5930679a987255741c8cc0f71afacd9814
Muhammad Anees <muhammadaneesrajpoot@gmail.com>
2024-06-11 09:28:00 +0500

    updated index.ts and added vercel.json

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,10 @@ import fastify from "fastify";
 import cors from "@fastify/cors";
 import { userRoutes } from "./routes/userRoutes";
 const app = fastify();
+app.get("/",(req,res)=>{
+  res.send("Hellow from server")
+})
+
 app.register(cors, { origin: process.env.CLIENT_URL });
 app.register(userRoutes);
 app.listen({ port: parseInt(process.env.PORT!) }, () =>
--- server/server.ts
+++ server/server.ts
@@ -3,8 +3,5 @@ dotenv.config()
 import fastify from "fastify"
 const app=fastify()
 
-app.get("/",(req,res)=>{
-    res.send("Hellow from server")
-})
 
 app.listen({port:parseInt(process.env.PORT!)})
